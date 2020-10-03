9351e1faa67840d95d59bf2ae9799b60f296c258
Paweł Szaga <Pawel.Szaga@luxmed.pl>
2020-10-04 00:24:49 +0200

    New version of script for automatic creating VM's under KVM

--- vm-create.sh
+++ vm-create.sh
@@ -7,6 +7,11 @@
 #folders
 KVM_VM_FOLDER="/home/pawel/KVM_VMs"
 ISO_FOLDER="/home/pawel/ISO"
+SSH_KEY_FILE="/home/pawel/.ssh/id_rsa.pub"
+
+#auth
+PASSWORD="password"
+SSH_KEY=`cat $SSH_KEY_FILE`
 
 #VM parameters
 VM_DISK_SIZE="128G"
@@ -18,57 +23,54 @@ VM_BRIDGE="virbr1"
 #display script usage
 if [ $# != 3 ]
 then
-	echo "Usage: $0 OSFAMILY VMNAME IPADDR"
+	echo "Usage: $0 OSFAMILY IPADDR VMNAME"
 	exit
 fi
 
 
 #getting variables from CLI parameters
 OSFAMILY=$1
-VMNAME=$2
-IPADDR=$3
+VMNAME=$3
+IPADDR=$2
 
 
-#list at:
-#osinfo-query os
-#
-#CentOS7 - working fine
-#OpenSUSE - IP is set, password changed, need to perform "zypper updage" before first reboot (without update there is kernel panic after reboot)
-#Ubuntu - IP not set, using DHCP
-#Debian8.6.1 - password not changed, no SSH access, no static IP, cloud-init not starting
-#after VM creation boot from live DVD in rescue mode, then user passwd utility, then regenerate RSA and DSA keys for sshd
-#
+#list at: osinfo-query os
+#CentOS7/8 - working fine
+#Fedora32 - working fine
+#OpenSUSE15.2 - working fine, DNS is not set
+#Ubuntu20.04 - unable to boot (grub rescue)
+#Debian10 - unable to boot (grub rescue)
 
 #choosing image files for OS Family
 case "$OSFAMILY" in
 "centos")  echo "OS: CentOS"
-	VM_OS_VARIANT="centOS7.0"
+	VM_OS_VARIANT="centos8"
 	CLOUD_IMAGE_FOLDER="$ISO_FOLDER/CentOS"
-	CLOUD_IMAGE_FILE="CentOS-7-x86_64-GenericCloud-1701.qcow2"
+	CLOUD_IMAGE_FILE="CentOS-8-GenericCloud-8.2.2004-20200611.2.x86_64.qcow2"
 	IFNAME="eth0"
 	;;
 "fedora")  echo "OS: Fedora"
-	VM_OS_VARIANT="fedora23"
+	VM_OS_VARIANT="fedora32"
 	CLOUD_IMAGE_FOLDER="$ISO_FOLDER/Fedora"
-	CLOUD_IMAGE_FILE="Fedora-Cloud-Base-25-20161121.0.x86_64.qcow2"
+	CLOUD_IMAGE_FILE="Fedora-Cloud-Base-32-1.6.x86_64.qcow2"
 	IFNAME="eth0"
 	;;
 "opensuse")  echo "OS: OpenSUSE"
-	VM_OS_VARIANT="openSUSE13.2"
+	VM_OS_VARIANT="opensuse15.1"
 	CLOUD_IMAGE_FOLDER="$ISO_FOLDER/OpenSUSE"
-	CLOUD_IMAGE_FILE="openSUSE-Leap-42.1-OpenStack.x86_64-0.0.4-Build2.88.qcow2"
+	CLOUD_IMAGE_FILE="openSUSE-Leap-15.2-OpenStack.x86_64.qcow2"
 	IFNAME="eth0"
 	;;
 "ubuntu") echo "OS: Ubuntu"
-	VM_OS_VARIANT="ubuntu14.04"
+	VM_OS_VARIANT="ubuntu20.04"
 	CLOUD_IMAGE_FOLDER="$ISO_FOLDER/Ubuntu"
-	CLOUD_IMAGE_FILE="xenial-server-cloudimg-amd64-disk1.img"
+	CLOUD_IMAGE_FILE="focal-server-cloudimg-amd64.img"
 	IFNAME="ens3"
 	;;
 "debian") echo  "OS: Debian"
-	VM_OS_VARIANT="debian8"
+	VM_OS_VARIANT="debian10"
 	CLOUD_IMAGE_FOLDER="$ISO_FOLDER/Debian"
-	CLOUD_IMAGE_FILE="debian-8.7.1-20170215-openstack-amd64.qcow2"
+	CLOUD_IMAGE_FILE="debian-10-genericcloud-amd64-20200610-293.qcow2"
 	IFNAME="eth0"
 	;;
 *) echo "OS: Unknown"
@@ -92,19 +94,35 @@ qemu-img create -f qcow2 $KVM_VM_FOLDER/$VMNAME.qcow2 $VM_DISK_SIZE
 printf "\nCopying data to new qcow image...\n"
 virt-resize --expand /dev/sda1 $CLOUD_IMAGE_FOLDER/$CLOUD_IMAGE_FILE $KVM_VM_FOLDER/$VMNAME.qcow2
 
+
 #creating user-data for cloud-init
 cat > $KVM_VM_FOLDER/tmp/user-data <<EOL
 #cloud-config
-password: password
+
+timezone: "Europe/Warsaw"
+
+users:
+  - name: visit
+    groups: wheel
+    ssh-authorized-keys:
+      - $SSH_KEY
+
+  - name: root
+    ssh-authorized-keys:
+      - $SSH_KEY
+
+password: $PASSWORD
 chpasswd:
   list: |
-    root:password
+    visit:$PASSWORD
+    root:$PASSWORD
   expire: False
 ssh_pwauth: True
 disable_root: false
 
-#below workaround for Ubuntu (setting SSHD: PermitRootLogin yes)
 runcmd:
+- sudo touch /etc/cloud/cloud-init.disabled
+#below workaround for Ubuntu (setting SSHD: PermitRootLogin yes)
 - sed -i'.orig' -e's/prohibit-password/yes/' /etc/ssh/sshd_config
 - service sshd restart
 EOL
@@ -135,6 +153,10 @@ EOL
 #creating ISO file with config files for cloud-init
 genisoimage -output $KVM_VM_FOLDER/tmp/$VMNAME-cidata.iso -volid cidata -joliet -rock $KVM_VM_FOLDER/tmp/user-data $KVM_VM_FOLDER/tmp/meta-data
 
+#remove config files after ISO creation
+rm $KVM_VM_FOLDER/tmp/user-data
+rm $KVM_VM_FOLDER/tmp/meta-data
+
 #installing and running new VM in KVM
 virt-install --noautoconsole --ram $VM_RAM --vcpus $VM_VCPUS --accelerate -n $VMNAME --disk $KVM_VM_FOLDER/$VMNAME.qcow2 --bridge $VM_BRIDGE --import --os-variant $VM_OS_VARIANT --disk $KVM_VM_FOLDER/tmp/$VMNAME-cidata.iso,device=cdrom
 
@@ -159,7 +181,7 @@ echo 'Staring arp-scan...'
 while :
 do
 	#arp-scan for particular MAC
-	data=`arp-scan -I $VM_BRIDGE --local | grep $MACADDR`
+	data=`arp-scan -I $VM_BRIDGE --local -B 10M -t 250 | grep $MACADDR`
 
 	#when MAC is found process returns 0, when MAC is NOT found process returns 1
 	if [[ $? == 0 ]]; then break; fi
@@ -173,12 +195,24 @@ printf "\nFound:\n"
 echo $data
 
 
+#unmount ISO
+printf "\nUnmounting temp ISO image: $VMNAME-cidata.iso\n"
+
+# get name of target path
+targetDrive=$(virsh domblklist $VMNAME | grep $VMNAME-cidata.iso | awk {' print $1 '})
+
+# force ejection of CD
+virsh change-media $VMNAME --path $targetDrive --eject --force --config
+
+#remove temp ISO
+rm $KVM_VM_FOLDER/tmp/$VMNAME-cidata.iso
+
 #getting IP ADDRESS
 IPADDR=`echo $data | awk '{print $1}'`
 
 printf "\nTrying to ping VM...\n"
 
-ping -c 3 $IPADDR
+ping -i 0.2 -c 5 $IPADDR
 
 printf "\nSSH connect command:\n"
 
