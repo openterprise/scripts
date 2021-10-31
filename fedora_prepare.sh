17064bc4599f22043c9d3f76d84fbe4e47a7a221
Paweł Szaga <Pawel.Szaga@luxmed.pl>
2021-10-31 11:49:02 +0100

    add nextcloud-client-nautilus

--- fedora_prepare.sh
+++ fedora_prepare.sh
@@ -3,4 +3,5 @@ dnf update
 dnf install gnome-power-manager gnome-tweaks oxygen-icon-theme dnf-plugin-system-upgrade powertop \
 bat vim htop iotop curl wget links elinks wavemon mc minicom putty arp-scan kernel-tools make g++ golang \
 ansible wireshark NetworkManager-fortisslvpn-gnome virt-manager qemu-kvm libguestfs-tools virt-install virt-what gparted \
-nextcloud-client p7zip-gui xsane tilix tilix-nautilus gimp audacity okular keepassxc filezilla kleopatra thunderbird remmina shutter
+nextcloud-client nextcloud-client-nautilus p7zip-gui xsane tilix tilix-nautilus gimp audacity okular \
+keepassxc filezilla kleopatra thunderbird remmina shutter
